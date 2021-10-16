0bf6805b526bfa1d99fba69dc3bd685153bac602
Madis Pink <madis.pink@gmail.com>
2021-10-16 23:51:03 +0300

    fix: improve caching a bit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
+import { existsSync, promises } from 'fs';
+import { env } from 'process';
+
 import { addPath, exportVariable, getInput, setFailed } from '@actions/core';
 import { exec } from '@actions/exec';
 import { cacheFile, downloadTool, find } from '@actions/tool-cache';
-
-import { promises } from 'fs';
-import { env } from 'process';
 
 const EW_CLI_URL = "https://maven.emulator.wtf/releases/ew-cli";
 
@@ -12,12 +12,21 @@ export default async function setup() {
     const version = getInput('version');
     exportVariable('EW_VERSION', version);
 
-    const cachedCli = find('emulatorwtf-wrapper', version);
-    if (!cachedCli) {
-      const path = await downloadTool(EW_CLI_URL);
-      cacheFile(path, 'ew-cli', 'emulatorwtf-wrapper', version);
-      addPath(path);
+    const binPath = `${env.HOME}/.cache/emulator-wtf/bin`;
+    await promises.mkdir(binPath, { recursive: true });
+
+    const executable = `${binPath}/ew-cli`;
+    if (existsSync(executable)) {
+      const cachedCli = find('emulatorwtf-wrapper', version);
+      if (!cachedCli) {
+        const path = await downloadTool(EW_CLI_URL);
+        cacheFile(path, 'ew-cli', 'emulatorwtf-wrapper', version);
+        await promises.copyFile(path, executable);
+      } else {
+        await promises.copyFile(cachedCli, executable);
+      }
     }
+    addPath(binPath);
 
     const cachedJar = find('emulatorwtf-jar', version);
     if (cachedJar) {
@@ -26,7 +35,9 @@ export default async function setup() {
 
     await exec('ew-cli --version');
 
-    cacheFile(`${env.HOME}/.cache/emulator-wtf/ew-cli-${version}.jar`, 'ew-cli.jar', 'emulatorwtf-jar', version);
+    if (!cachedJar) {
+      cacheFile(`${env.HOME}/.cache/emulator-wtf/ew-cli-${version}.jar`, 'ew-cli.jar', 'emulatorwtf-jar', version);
+    }
   } catch (e) {
     setFailed(e);
   }
