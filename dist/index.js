637140b1dc8e664b06662cf83b35d54e0629338f
Madis Pink <mail@madis.pink>
2023-04-22 00:06:45 +0300

    chore: rebuild project

--- dist/index.js
+++ dist/index.js
@@ -35,7 +35,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
 var process_1 = require("process");
 var core_1 = require("@actions/core");
